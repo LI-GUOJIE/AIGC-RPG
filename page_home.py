148205b6d96007f23419621409756b71e4b487c8
EGG_PC\egg <imliguojie@gmail.com>
2023-06-13 18:59:50 +0800

    Rename titile

--- page_home.py
+++ page_home.py
@@ -3,7 +3,7 @@ from page_template import load_page_template
 import gradio as gr
 
 # 静态主页
-with gr.Blocks(title="RecurrentGPT", css="footer {visibility: hidden}", theme="default") as demo:
+with gr.Blocks(title="AIRPG", css="footer {visibility: hidden}", theme="default") as demo:
     with gr.Tab("模板管理页面"):
         load_page_template()
     with gr.Tab("玩家页面"):
